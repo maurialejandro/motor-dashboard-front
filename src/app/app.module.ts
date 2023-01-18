49940f23a47151afc8ca26f03f46a9e01e0470f1
flyingbiker <mauri.alejandro2165@gmail.com>
2023-01-18 12:32:09 -0300

    servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from "@angular/material/icon"
+import { MatMenuModule } from '@angular/material/menu';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { ListServiciosComponent } from './list-servicios/list-servicios.component';
+import { BarComponent } from './bar/bar.component';
+import { AlertComponent } from './alert/alert.component'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServiciosComponent,
+    ListServiciosComponent,
+    BarComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
@@ -17,7 +26,8 @@ import { MatIconModule } from "@angular/material/icon"
     BrowserAnimationsModule,
     MatSlideToggleModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/list-servicios/list-servicios.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-servicios',
+  templateUrl: './list-servicios.component.html',
+  styleUrls: ['./list-servicios.component.scss']
+})
+export class ListServiciosComponent {
+
+}
